0a02cb79d14dc885d5977467aaa4f05f75f9a8fb
sanauwar khan <sanauwarkhan84@gmail.com>
2026-05-17 10:46:39 +0530

    fix

--- app.js
+++ app.js
@@ -91,6 +91,10 @@ app.use((req,res,next) => {
     next();
 });
 
+app.get("/", (req,res) => {
+    res.redirect("/listings");
+});
+
 
  app.use("/listings", listingRouter);
  app.use("/listings/:id/reviews",reviewRouter);
